be720ad7565bf6b4bdfcc0a3fddbcdd954c257e1
Duncan.Pham <duncan.pham@vincere.io>
2023-08-08 15:23:28 +0700

    update code

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Use an Ubuntu base image
+FROM ubuntu:latest
+
+# Set environment variables for Spark and Hadoop versions
+ENV SPARK_VERSION=3.3.2
+ENV HADOOP_VERSION=3
+
+# Install Java (required for Spark)
+RUN apt-get update && apt-get install -y openjdk-8-jdk
+
+# Download and extract Spark
+RUN apt-get install -y wget
+RUN wget https://dlcdn.apache.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+RUN tar -xzf spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+RUN mv spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION /opt
+RUN rm spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+
+# Set environment variables for Spark and Hadoop paths
+ENV SPARK_HOME=/opt/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION
+ENV PATH=$PATH:$SPARK_HOME/bin
+
+# Set working directory
+WORKDIR /app
+
+# Copy files
+COPY requirements.txt .
+COPY etl.py .
+
+# Install package with pip
+RUN apt-get install -y python3 python3-pip
+RUN pip install -r requirements.txt
+
+# Run container as an executable
+CMD ["python3", "etl.py"]
--- etl.py
+++ etl.py
@@ -8,125 +8,123 @@ import json
 year = 2022
 
 
-def init_spark():
-    try:
-        os.makedirs('driver')
-    except OSError as err:
-        print(f'Error: {err}')
+def download_files():
+	try:
+		os.makedirs('driver')
+		os.makedirs('data/green_taxi')
+		os.makedirs('data/yellow_taxi')
+	except OSError as err:
+		print(f'Error: {err}')
 
-    os.system(
-        'curl -o driver/postgresql-42.6.0.jar https://jdbc.postgresql.org/download/postgresql-42.6.0.jar')
-    spark = SparkSession.builder \
-        .appName("ETL-with-spark") \
-        .config("spark.jars", "driver/postgresql-42.6.0.jar") \
-        .getOrCreate()
-    return spark
+	os.system(
+		'curl -o app/driver/postgresql-42.6.0.jar https://jdbc.postgresql.org/download/postgresql-42.6.0.jar')
+	os.system(
+		'curl -o app/data/taxi_zone_lookup.csv https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv')
+	
+	print('Start download parquet files')
+	for i in range(1, 3):
+		month = f'{i:02}'
+		url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{0}-{1}.parquet'.format(
+			year, month)
+		csv_name = 'app/data/green_taxi/green_tripdata_{0}-{1}.parquet'.format(
+			year, month)
+		os.system(f'curl -o {csv_name} {url}')
+
+	for i in range(1, 3):
+		month = f'{i:02}'
+		url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{0}-{1}.parquet'.format(
+			year, month)
+		csv_name = 'app/data/yellow_taxi/yellow_tripdata_{0}-{1}.parquet'.format(
+			year, month)
+		os.system(f'curl -o {csv_name} {url}')
+	print('Download successfully')
+
+
+def init_spark():
+	spark = SparkSession.builder \
+		.appName("ETL-with-spark") \
+		.config("spark.jars", "app/driver/postgresql-42.6.0.jar") \
+		.getOrCreate()
+	return spark
 
 
 def extract(spark):
-    os.system(
-        'curl -o data/taxi_zone_lookup.csv https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv')
-
-    try:
-        os.makedirs('data/green_taxi')
-        os.makedirs('data/yellow_taxi')
-    except OSError as err:
-        print(f'Error: {err}')
-
-    print('Start download parquet files')
-    for i in range(1, 3):
-        month = f'{i:02}'
-        url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{0}-{1}.parquet'.format(
-            year, month)
-        csv_name = 'data/green_taxi/green_tripdata_{0}-{1}.parquet'.format(
-            year, month)
-        os.system(f'curl -o {csv_name} {url}')
-
-    for i in range(1, 3):
-        month = f'{i:02}'
-        url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{0}-{1}.parquet'.format(
-            year, month)
-        csv_name = 'data/yellow_taxi/yellow_tripdata_{0}-{1}.parquet'.format(
-            year, month)
-        os.system(f'curl -o {csv_name} {url}')
-
-    print('Download successfully')
-    green_taxi = spark.read.parquet('data/green_taxi')
-    yellow_taxi = spark.read.parquet('data/yellow_taxi')
-
-    os.system(f'curl -o data/taxi_zone_lookup.csv https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv')
-    df_zone = spark.read.csv('data/taxi_zone_lookup.csv',
-                             inferSchema=True, header=True).filter(col('Borough') != 'Unknown')
-    return [green_taxi, yellow_taxi], df_zone
+	green_taxi = spark.read.parquet('app/data/green_taxi')
+	yellow_taxi = spark.read.parquet('app/data/yellow_taxi')
+	df_zone = spark.read.csv('app/data/taxi_zone_lookup.csv',
+							 inferSchema=True, header=True).filter(col('Borough') != 'Unknown')
+	return [green_taxi, yellow_taxi], df_zone
 
 
 def generate_dim_table(df):
-    for i in ['passenger_count', 'RateCodeID', 'payment_type', 'trip_type']:
-        df = df.withColumn(i, col(i).cast(IntegerType()))
-    payment_type_mapping = when(col('id') == 1, 'Credit card'). \
-        when(col('id') == 2, 'Cash'). \
-        when(col('id') == 3, 'No charge'). \
-        when(col('id') == 4, 'Dispute'). \
-        when(col('id') == 5, 'Unknown').otherwise('Voided trip')
-    df_payment = df.select('payment_type').withColumnRenamed(
-        'payment_type', 'id').distinct().dropna().orderBy(col('id'))
-    df_payment = df_payment.withColumn('payment_type', payment_type_mapping)
+	for i in ['passenger_count', 'RateCodeID', 'payment_type', 'trip_type']:
+		df = df.withColumn(i, col(i).cast(IntegerType()))
+	payment_type_mapping = when(col('id') == 1, 'Credit card'). \
+		when(col('id') == 2, 'Cash'). \
+		when(col('id') == 3, 'No charge'). \
+		when(col('id') == 4, 'Dispute'). \
+		when(col('id') == 5, 'Unknown').otherwise('Voided trip')
+	df_payment = df.select('payment_type').withColumnRenamed(
+		'payment_type', 'id').distinct().dropna().orderBy(col('id'))
+	df_payment = df_payment.withColumn('payment_type', payment_type_mapping)
 
-    rate_code_mapping = when(col('id') == 1, 'Standard rate'). \
-        when(col('id') == 2, 'JFK'). \
-        when(col('id') == 3, 'Newark'). \
-        when(col('id') == 4, 'Nassau or Westchester'). \
-        when(col('id') == 5, 'Negotiated fare').otherwise('Group ride')
-    df_ratecode = df.select('RateCodeID').withColumnRenamed(
-        'RateCodeID', 'id').distinct().dropna().orderBy(col('id'))
-    df_ratecode = df_ratecode.withColumn('rate_type', rate_code_mapping)
+	rate_code_mapping = when(col('id') == 1, 'Standard rate'). \
+		when(col('id') == 2, 'JFK'). \
+		when(col('id') == 3, 'Newark'). \
+		when(col('id') == 4, 'Nassau or Westchester'). \
+		when(col('id') == 5, 'Negotiated fare').otherwise('Group ride')
+	df_ratecode = df.select('RateCodeID').withColumnRenamed(
+		'RateCodeID', 'id').distinct().dropna().orderBy(col('id'))
+	df_ratecode = df_ratecode.withColumn('rate_type', rate_code_mapping)
 
-    df_vendor = df.select('VendorID').withColumnRenamed(
-        'VendorID', 'id').distinct().dropna().orderBy(col('id'))
-    df_vendor = df_vendor.withColumn('vendor', when(col('id') == 1, 'Creative Mobile Technologies, LLC').
-                                     when(col('id') == 2, 'VeriFone Inc.'))
-    return [df_payment, df_ratecode, df_vendor]
+	df_vendor = df.select('VendorID').withColumnRenamed(
+		'VendorID', 'id').distinct().dropna().orderBy(col('id'))
+	df_vendor = df_vendor.withColumn('vendor', when(col('id') == 1, 'Creative Mobile Technologies, LLC').
+									 when(col('id') == 2, 'VeriFone Inc.'))
+	return [df_payment, df_ratecode, df_vendor]
 
 
 def transform(df):
-    trip_time_in_mins = unix_timestamp(
-        col('lpep_dropoff_datetime')) - unix_timestamp(col('lpep_pickup_datetime'))
-    df = df.withColumn('trip_time_in_mins', round(trip_time_in_mins / 60, 2))
-    return df
+	trip_time_in_mins = unix_timestamp(
+		col('lpep_dropoff_datetime')) - unix_timestamp(col('lpep_pickup_datetime'))
+	df = df.withColumn('trip_time_in_mins', round(trip_time_in_mins / 60, 2))
+	return df
 
 
 def load(df, table_name: str):
-    # Read credentials from the config file
-    with open('config.json', 'r') as config_file:
-        config_data = json.load(config_file)
-    host = config_data['host']
-    port = config_data['port']
-    database = config_data['database']
-    user = config_data['user']
-    password = config_data['password']
+	# Read credentials from the config file
+	with open('config.json', 'r') as config_file:
+		config_data = json.load(config_file)
+	host = config_data['host']
+	port = config_data['port']
+	database = config_data['database']
+	user = config_data['user']
+	password = config_data['password']
 
-    url = 'jdbc:postgresql://{0}:{1}/{2}'.format(host, port, database)
-    properties = {
-        'user': user,
-        'password': password,
-        'driver': 'org.postgresql.Driver'}
-    df.write.jdbc(url=url, table=table_name, mode='overwrite',
-                  properties=properties)
-    print(f'Insert data into {table_name} successfully!')
+	url = 'jdbc:postgresql://{0}:{1}/{2}'.format(host, port, database)
+	properties = {
+		'user': user,
+		'password': password,
+		'driver': 'org.postgresql.Driver'}
+	df.write.jdbc(url=url, table=table_name, mode='overwrite',
+				  properties=properties)
+	print(f'Insert data into {table_name} successfully!')
+
 
 
 def etl_main():
-    spark = init_spark()
-    dfs, df_zone = extract(spark)
-    dim_tables = generate_dim_table(dfs[0])
-    dim_tables_name = ['payment', 'ratecode', 'vendor']
-    for tbls, name in zip(dim_tables, dim_tables_name):
-        load(tbls, name)
-    load(df_zone, 'taxi_zone_lookup')
+	download_files()
+	spark = init_spark()
+	dfs, df_zone = extract(spark)
+	dim_tables = generate_dim_table(dfs[0])
+	dim_tables_name = ['payment', 'ratecode', 'vendor']
+	for tbls, name in zip(dim_tables, dim_tables_name):
+		load(tbls, name)
+	load(df_zone, 'taxi_zone_lookup')
 
-    tables_name = ['green_taxi', 'yellow_taxi']
-    for df, name in zip(dfs, tables_name):
-        load(df, name)
-        
+	tables_name = ['green_taxi', 'yellow_taxi']
+	for df, name in zip(dfs, tables_name):
+		load(df, name)
+
 
 etl_main()
